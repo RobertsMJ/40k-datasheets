e0f8eb80a5bed2f76e4f80936db27059c81e19ea
mitch <robertsmj1138@gmail.com>
2023-12-23 21:16:19 -0500

    playing around with refs and reactives

--- src/views/DatasheetsView.vue
+++ src/views/DatasheetsView.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
 import type { IDatasheet } from '@/types/datasheets'
 import DataSheet from '@/components/DataSheet.vue'
+import { reactive } from 'vue'
 
 const data = await fetch('space_marines.json')
 const datasheets: IDatasheet[] = await data.json()
+
+let form = reactive({ name: '' })
+const onClick = () => {
+  console.log(form)
+}
 </script>
 
 <template>
   <main>
-    <DataSheet v-for="data in datasheets" :data="data" :key="data.name"></Datasheet>
+    <form>
+      <input type="text" v-model="form.name" />
+      <p>name is: {{ form.name }}</p>
+      <button type="button" @click="onClick">click me</button>
+    </form>
+
+    <DataSheet v-for="data in datasheets" :data="data" :key="data.name"></DataSheet>
   </main>
 </template>
