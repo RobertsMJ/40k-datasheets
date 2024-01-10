d8bb0aa380daefc4a888aa9919c7d856233ea7f1
mitch <robertsmj1138@gmail.com>
2024-01-10 11:51:24 -0500

    rewrite store refactor around actually using store, end up right back where I started

--- src/store/datasheets.ts
+++ src/store/datasheets.ts
@@ -1,19 +1,24 @@
 import type { IDatasheet } from '@/types/datasheets'
 import { defineStore } from 'pinia'
+import { ref, type MaybeRefOrGetter, computed, toValue, type Ref } from 'vue'
 
-export const useDataSheetsStore = defineStore('datasheets', {
-  state: () => ({ data: [] as IDatasheet[] }),
-  getters: {
-    dataSheets: (state) => state.data
-  },
-  actions: {
-    async getDataSheets() {
-      try {
-        const data = await fetch('space_marines.json')
-        this.data = await data.json()
-      } catch (ex) {
-        console.error(ex)
-      }
+export const useDatasheetsStore = defineStore('datasheets', () => {
+  const data = ref<IDatasheet[]>([])
+  const datasheets = computed((): Ref<IDatasheet[]> => data)
+
+  async function getDatasheets(faction: MaybeRefOrGetter<string>) {
+    try {
+      const res = await fetch(`${toValue(faction)}.json`)
+      data.value = await res.json()
+    } catch (ex) {
+      if (ex instanceof Error) throw ex
+      else throw new Error(String(ex))
     }
   }
+
+  function addDatasheet(datasheet: IDatasheet) {
+    data.value.push(datasheet)
+  }
+
+  return { datasheets, getDatasheets, addDatasheet }
 })
--- src/views/DatasheetsView.vue
+++ src/views/DatasheetsView.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import DataSheet from '@/components/DataSheet.vue'
-import { useFetch } from '@/composables/useFetch'
 import { useCursor } from '@/composables/useCursor'
-import type { IDatasheet } from '@/types/datasheets'
 import { ref } from 'vue'
+import { useFetch } from '@/composables/useFetch'
+import type { IDatasheet } from '@/types/datasheets'
 
 const { x, y } = useCursor()
 
 const faction = ref('space_marines')
-const { data, error } = useFetch<IDatasheet[]>(() => `${faction.value}.json`)
+const { data: datasheets, error } = useFetch<IDatasheet[]>(
+  () => `${faction.value}.json`
+)
 </script>
 
 <template>
@@ -19,12 +21,14 @@ const { data, error } = useFetch<IDatasheet[]>(() => `${faction.value}.json`)
       <option value="adeptus_mechanicus">Adeptus Mechanicus</option>
       <option value="tyranids">Tyranids</option>
     </select>
-    <div v-if="error">Select a faction! {{ error }}</div>
-    <DataSheet
-      v-else
-      v-for="sheet in data"
-      :data="sheet"
-      :key="sheet.name"
-    ></DataSheet>
+    <section>
+      <div v-if="error">Select a faction! {{ error }}</div>
+      <DataSheet
+        v-else
+        v-for="sheet in datasheets"
+        :data="sheet"
+        :key="sheet.name"
+      ></DataSheet>
+    </section>
   </main>
 </template>
